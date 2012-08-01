65d3fd224c7fb2ee0a7a5cbadeb0c6459b7c0b3d
Chris Anderson <chris.anderson@livingsocial.com>
2012-07-31 20:45:19 -0400

    Added check on appending digits

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -9,13 +9,23 @@
 #import "CalculatorViewController.h"
 
 @interface CalculatorViewController ()
-
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
 @end
 
 @implementation CalculatorViewController
+
 @synthesize display;
+@synthesize userIsInTheMiddleOfEnteringANumber;
 
 - (IBAction)digitPressed:(UIButton *)sender
 {
+    NSString *digit = sender.currentTitle;
+    
+    if (self.userIsInTheMiddleOfEnteringANumber) {
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    } else {
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+    }
 }
 @end

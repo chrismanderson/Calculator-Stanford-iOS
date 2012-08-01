455d6e1fb42d4139cb07ac40f0708a9fbcd85ab7
Chris Anderson <chris.anderson@livingsocial.com>
2012-07-31 21:12:35 -0400

    Added operations

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -8,6 +8,52 @@
 
 #import "CalculatorBrain.h"
 
+@interface CalculatorBrain()
+@property (nonatomic, strong) NSMutableArray *operandStack;
+@end
+
 @implementation CalculatorBrain
+
+@synthesize operandStack = _operandStack;
+
+- (NSMutableArray *)operandStack
+{
+    if (!_operandStack) {
+        _operandStack = [[NSMutableArray alloc] init];
+    }
+    return _operandStack;
+}
+
+- (double)popOperand
+{
+    NSNumber *operandObject = [self.operandStack lastObject];
+    if (operandObject) [self.operandStack removeLastObject];
+    return [operandObject doubleValue];
+}
+
+- (void)pushOperand:(double)operand
+{
+    NSNumber *operandObject = [NSNumber numberWithDouble:operand];
+    [self.operandStack addObject:operandObject];
+}
+- (double)performOperation:(NSString *)operation
+{
+    double result = 0;
+    
+    if ([operation isEqualToString:@"+"]) {
+        result = [self popOperand] + [self popOperand];
+    } else if ([@"*" isEqualToString:operation]) {
+        result = [self popOperand] * [self popOperand];
+    } else if ([operation isEqualToString:@"-"]) {
+        double subtrahend = [self popOperand];
+        result = [self popOperand] - subtrahend;
+    } else if ([operation isEqualToString:@"/"]) {
+        double divisor = [self popOperand];
+        if (divisor) result = [self popOperand] / divisor;
+    }
+    
+    [self pushOperand:result];
+    return result;
+}
 
 @end

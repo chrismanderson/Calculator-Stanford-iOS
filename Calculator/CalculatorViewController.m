825cf80d3ee7afe56213ec15bfccf6a02599fd37
Chris Anderson <chris.anderson@livingsocial.com>
2012-07-31 20:33:09 -0400

    Added buttons to ui

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -13,22 +13,9 @@
 @end
 
 @implementation CalculatorViewController
+@synthesize display;
 
-- (void)viewDidLoad
+- (IBAction)digitPressed:(UIButton *)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
-
 @end
